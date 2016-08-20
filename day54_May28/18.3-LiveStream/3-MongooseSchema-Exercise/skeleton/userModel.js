230031abd96012f880af45c3a131e444a3e15906
Lee Warnock <clwarnock@gmail.com>
2016-08-20 10:48:48 -0500

    adds skeleton 1 ME

--- day54_May28/18.3-LiveStream/3-MongooseSchema-Exercise/skeleton/userModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var UserSchema = new Schema({
-  //Code goes in here
-});
-
-var User = mongoose.model('User', UserSchema);
-module.exports = User;
