fa7d639c25febbf5286a52bc1c1147fab5c26e30
Lee Warnock <clwarnock@gmail.com>
2016-08-29 19:42:12 -0500

    solutions 1 and 2

---
+++ day57_Jun6/PHP activity 1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$firstName = "Lee";
+$lastName = Warnock;
+$birthYear = 10.5;
+$full = $fist . $last;
+$currentYear = 2016;
+$age = $currentYear - $birthYear;
+ echo "my name is $full"
+echo "I am $ years old"
+
+if 
+?>
+
+
+
+</body>
+</html>	
