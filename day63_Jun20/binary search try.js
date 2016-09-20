ce53fb247600615e8078827a20afb256996ca032
Lee Warnock <clwarnock@gmail.com>
2016-09-19 19:08:09 -0500

    adds binary search functions

--- day63_Jun20/binary search try.js
+++ day63_Jun20/binary search try.js
@@ -1,22 +1,11 @@
 'use strict';
 
-// Benchmark is a library that times 
-var Benchmark = require('benchmark'),
- stuff = [10,34,56,67,93,120,137,168,259,280,311,342,413,514],
- random_value = stuff[ Math.ceil( Math.random() * 14 ) ];
 
-// A "suite" is a series of code snippets you
-//   want to execute and time.
-var suite = new Benchmark.Suite();
-
-suite
-
-  // Add the function 'linear_search' to the suite.
 var result = binarySearch( [1,23,43,56,77,89,211,212,789,972,1001,4567,4599,83784], 77        );
 
 console.log( result );
 
-.add('binarySearch', function binarySearch(stuff, searchElement) {
+function binarySearch(stuff, searchElement) {
 
     // Set some starting values.
     var minIndex = 0,
@@ -63,19 +52,3 @@ console.log( result );
     return false;
 
 }
-
-  // On 'start', run the 'start' function.
-  .on('start', function start () {
-    console.log('Beginning benchmark...');
-  })
-
-  // On the 'complete' event, run the 'report' function.
-  .on('complete', function report () {
-    // Get successful benchmark.
-    var benchmark = Benchmark.filter(this, 'successful')[0];
-
-    console.log('On average, ' + benchmark.name + ' took ' + benchmark.stats.mean + ' seconds to complete.');
-  })
-
-  // Run the test!
-  .run();
